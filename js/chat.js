ae1b9f04b92c78de7c56fc9e3a23dff35b117c64
kakashi's left eye <135690017+Fuffuhvg@users.noreply.github.com>
2024-06-11 15:10:52 +0300

    line graph

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 var xValues = ["A", "B", "C", "D", "E"];
 var yValues = [55, 49, 44, 24, 15];
-var barColors = ["red", "green","blue","orange","brown"];
+var barColors = ["red", "black","red","black","red"];
 
 new Chart("myChart", {
   type: "bar",
@@ -15,7 +15,35 @@ new Chart("myChart", {
     legend: {display: false},
     title: {
       display: true,
-      text: "TOTAL SALES BY PRODUCT QUANTITY + AMOUNT"
+      text: "BEST SELLING PRODUCT BY QUANTITY"
     }
   }
 });
+
+
+
+// Data for the line chart
+var lineXValues = ["January", "February", "March", "April", "May"];
+var lineYValues = [10000, 15000, 13000, 18000, 16000];
+
+new Chart("myLineChart", {
+  type: "line",
+  data: {
+    labels: lineXValues,
+    datasets: [{
+      label: 'Sales Amount (KSHS)',
+      fill: false,
+      borderColor: "black",
+      data: lineYValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "TOTAL SALES BY PRODUCT QUANTITY + AMOUNT OVER TIME"
+    }
+  }
+});
+
+
+
