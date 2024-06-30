7a1310a568dd54e067c8f02ec7bcc8e20e52a9f7
Fuffuhvg <jeffakalama@gmail.com>
2024-06-30 17:42:58 +0300

    toggler

--- js/theme.js
+++ js/theme.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const themeToggler = document.getElementById('theme-toggler');
     const btnText = document.getElementById('btnText');
     const body = document.body;
-  
+    const btnIcon = document.getElementById('btnIcon');
+
     themeToggler.addEventListener('click', () => {
       body.classList.toggle('dark-theme');
       if (body.classList.contains('dark-theme')) {
+        btnIcon.src = "img/sun.png";
         btnText.textContent = 'Light';
       } else {
+        btnIcon.src = "img/moon.png";
         btnText.textContent = 'Dark';
       }
     });
