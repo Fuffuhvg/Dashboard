5fd7df9b0c4d409c6a35fc43d79d46b6f2915dbf
Fuffuhvg <jeffakalama@gmail.com>
2024-07-17 09:48:54 +0300

    login

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
   wrapper.classList.add('active');
@@ -17,9 +16,6 @@ btnPopup.addEventListener('click', ()=> {
   wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click', () => {
-  wrapper.style.display = 'none'; // This will hide the wrapper
-});
 
 //sidebar js
 const sideMenu = document.querySelector("aside");
