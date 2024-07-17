3ea5c8e82bf10682e5b155671a036db8f949a372
Fuffuhvg <jeffakalama@gmail.com>
2024-07-17 10:34:08 +0300

    login

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//login js
+//login & logout js
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
